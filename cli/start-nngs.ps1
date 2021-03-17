22b58a4c347bac9ad3cae4b7ae240f5fb16660dd
むずでょ <muzudho1@gmail.com>
2021-03-17 19:12:05 +0900

    CLI

---
+++ cli/start-nngs.ps1
@@ -0,0 +1,11 @@
+﻿# UTF-8 with BOM
+
+# ファイルパスは書きかえてください
+Set-Location -Path "C:/Users/むずでょ/Documents/GitHub/kifuwarabe-collection/nngs-docker"
+
+$env:COMPOSE_PROJECT_NAME = "nngs"
+
+# 初回はこっち
+# Start-Process -FilePath "docker-compose.exe" -ArgumentList "up -d --build" -NoNewWindow -Wait
+# ビルド後はこっち
+Start-Process -FilePath "docker-compose.exe" -ArgumentList "up -d" -NoNewWindow -Wait
---
+++ cli/stop-nngs.ps1
@@ -0,0 +1,6 @@
+﻿# UTF-8 with BOM
+
+# ファイルパスは書きかえてください
+Set-Location -Path "C:/Users/むずでょ/Documents/GitHub/kifuwarabe-collection/nngs-docker"
+
+Start-Process -FilePath "docker-compose.exe" -ArgumentList "down" -NoNewWindow -Wait
